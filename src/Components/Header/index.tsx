9cb1289923d9d282027e179c54a81cb6c007c1d9
Eduarda Matos <duda.mdev@gmail.com>
2024-11-13 09:38:28 -0300

    Add hamburguer menu to smaller screens.

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import Logo from "../../Common/Logo";
 import Flag from '../../Common/Flag';
 import './style.css';
@@ -7,22 +8,32 @@ import { HeaderProps } from "../../Common/types";
 
 export default function Header({ scrollToSection, homeSection, aboutSection, howSection, servicesSection, contactSection, reference }: HeaderProps) {
     const { i18n, t } = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleChangeLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
     };
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header ref={reference}>
             <Logo />
-            <nav>
-                <div className='nav-container'>
+
+            <button className='hamburger' onClick={toggleMenu}>
+                &#9776;
+            </button>
+
+            <nav className={`nav-container ${isOpen ? 'open' : ''}`}>
+                
                     <a href="#" onClick={(e) => { e.preventDefault(); scrollToSection(homeSection); }}>{t("home")}</a>
                     <a href="#" onClick={(e) => { e.preventDefault(); scrollToSection(aboutSection); }}>{t("about")}</a>
                     <a href="#" onClick={(e) => { e.preventDefault(); scrollToSection(howSection); }}>{t("howItWorks")}</a>
                     <a href="#" onClick={(e) => { e.preventDefault(); scrollToSection(servicesSection); }}>{t("services")}</a>
                     <Button title={t("contact")} color="yellow-button" onClick={(e) => { e.preventDefault(); scrollToSection(contactSection); }} />
-                </div>
+
 
                 {/* Seletor de idioma com bandeiras */}
                 <div className="language-selector">
